79e743fa23585026906f25672a0292055d7219a5
vlad <oskin0425@gmail.com>
2024-08-06 20:47:18 +0400

    Created insight section and added swiper

---
+++ src/js/components/slider.js
@@ -0,0 +1,41 @@
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+export const useInsightSlider = () => {
+  new Swiper('.insight__slider', {
+    modules: [Navigation, Pagination],
+    slidesPerView: 'auto',
+    spaceBetween: 32,
+    loop: true,
+    centeredSlides: true,
+    pagination: {
+      el: '.insight__pagination',
+      dynamicBullets: true,
+    },
+    breakpoints: {
+      993: {
+        centeredSlides: false,
+      },
+    },
+  });
+};
+
+export const usePartnersSlider = () => {
+  new Swiper('.partners__slider', {
+    modules: [Navigation, Pagination],
+    slidesPerView: 1,
+    height: 'auto',
+    width: 160,
+    spaceBetween: 10,
+    loop: true,
+    centeredSlides: true,
+    breakpoints: {
+      993: {
+        centeredSlides: false,
+      },
+    },
+  });
+};
--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,9 @@ import '../assets/scss/main.scss';
 //components
 import { useTheme } from './components/theme.js';
 import { useBurger } from './components/burger.js';
+import { useInsightSlider, usePartnersSlider } from './components/slider.js';
 
 useTheme();
 useBurger();
+useInsightSlider();
+usePartnersSlider();
