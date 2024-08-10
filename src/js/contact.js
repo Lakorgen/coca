61e3be718a0080d1b26f7701871be2cb090a42d2
vlad <oskin0425@gmail.com>
2024-08-10 12:22:52 +0400

    Created contact:hero section and added contact:partners section

---
+++ src/js/contact.js
@@ -0,0 +1,10 @@
+import '../assets/scss/contact.scss';
+
+//components
+import { useTheme } from './components/theme.js';
+import { useBurger } from './components/burger.js';
+import { usePartnersSlider } from './components/home/slider.js';
+
+useTheme();
+useBurger();
+usePartnersSlider();
