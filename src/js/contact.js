b07b69dd1ccdc8ff7e2e66dc70d2c5b0919271c2
vlad <oskin0425@gmail.com>
2024-08-10 21:58:41 +0400

    Created contact:get-started section

--- src/js/contact.js
+++ src/js/contact.js
@@ -4,7 +4,11 @@ import '../assets/scss/contact.scss';
 import { useTheme } from './components/theme.js';
 import { useBurger } from './components/burger.js';
 import { usePartnersSlider } from './components/home/slider.js';
+import { useYandexMap } from './components/contact/map.js';
+import { usePhone } from './components/contact/phone.js';
 
 useTheme();
 useBurger();
 usePartnersSlider();
+useYandexMap();
+usePhone();
